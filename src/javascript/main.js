1e4145735833b9160775491ab7a42c7933a0ab5d
SamZCoder <coder.sam@outlook.com>
2020-04-14 01:24:01 +0200

    Fix Issue with child nodes for Dropdowns

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,6 +2,7 @@ console.log('Bulma Dashboard');
 //Sidebar Collapse
 Array.from(document.getElementById('sidebar').getElementsByClassName('has-dropdown')).forEach(element => {
     element.addEventListener('click', function(e) {
+        if(!e.target.parentNode.className.includes("has-dropdown")){return;}
         const submenu = element.getElementsByTagName('ul')[0];
         if(submenu != undefined)
         { submenu.style.display = submenu.style.display == "block" ? "none" : "block";}
